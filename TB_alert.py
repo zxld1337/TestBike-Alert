cd4ebd13dbf7bed3c2604dfba872fadf6db5635e
Silkó Levente <65249451+zxld1337@users.noreply.github.com>
2020-12-31 15:51:17 +0100

    Update TB_alert.py

--- TB_alert.py
+++ TB_alert.py
@@ -8,19 +8,19 @@ URL = 'https://bolha.testbike.hu/dh-fr-ossztelos'
 # User Agent
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/206.190.33.156 Safari/537.36'}
 
-# Funkció ami akkor lép életbe ha van új termék. 
+# Function that activates when a new bike is available - Funkció ami akkor lép életbe ha van új bicó. 
 def OpenTB():
     tb = "https://bolha.testbike.hu/dh-fr-ossztelos"
-    # Megnyitja az oldalt
+    # Opens the site - Megnyitja az oldalt
     webbrowser.open(tb)
 
 one = 1
-# Loop ami megnézi hogy változótt e a hirdetések száma
+# Loop which looks at how this number of ads is - Loop ami megnézi hogy változótt-e a hirdetések száma
 while True:
-    # Lekér minden infót az adott weblaprol
+    # Retrieves information from that web page - Lekéri az infót az adott weblapról
     page = requests.get(URL, headers=headers)
     soup = BeautifulSoup(page.content, "html.parser")
-    # Megkeresi mennyi hirdetés érhető el
+    # Finds how many ads are available - Megkeresi mennyi hirdetés érhető el
     title = soup.find(class_="d_inline_m m_right_17").get_text()
     hirdetes = (title[6:9])
     Hdb = int(hirdetes)
@@ -28,12 +28,12 @@ while True:
     if 1 == one:
         Hszam = int(hirdetes)
         one += 1
-    # Ha váltózott nyissa meg az oldalt
+    # If changed, open the page - Ha váltózott nyissa meg az oldalt
     if Hdb > Hszam:
         print("Új bicó elérhető!")
         print(f"Hirdetések száma: {hirdetes}")
         OpenTB()
-    # Ha nem, print
+    # If not print the following text -  Ha nem, irja ki a az adott szöveget
     else:
         t = time.localtime()
         current_time = time.strftime("%H:%M", t)
